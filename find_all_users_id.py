75e2c12d2fc4c1ddefc3291c976f072489fb3292
tajriba <quvvatullayev@gmail.com>
2022-07-18 09:12:58 +0300

    find_all_users_id.py

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -12,8 +12,8 @@ def find_all_users_id(data: dict)->list:
     list_print = []
     for i in data["messages"]:
         if i == 'id':
-            if i['actor_id'] not in list_print:
-                list_print.append([i['actor_id']])
+            if i['id'] not in list_print:
+                list_print.append(i['id'])
 
     return list_print
 print(find_all_users_id(read_data))
